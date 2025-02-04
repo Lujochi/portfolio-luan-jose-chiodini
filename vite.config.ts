c365417250464714b067af12252d1ad34f2e4c43
Luan Chiodini <a.luanjc@gmail.com>
2025-02-04 08:08:04 -0300

    Config deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio-luan-jose-chiodini/",
   plugins: [react()],
+  base: "/portfolio-luan-jose-chiodini",
 });
